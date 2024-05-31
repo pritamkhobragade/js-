f1b7e8a5cf677787f9bdd55b9c6daabdbcf1e6c2
Pritam Khobragade <165365406+pritamkhobragade@users.noreply.github.com>
2024-05-31 07:53:01 +0000

    container config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 123;
+let email = "pritam@gmail.com";
+var accountPassword = "12345";
+accountCity = "nagpur";
+let accountState;
+
+email = "khobragade@gmail.com";
+accountPassword = "9087";
+accountCity = "chandrapur";
+
+
+console.table([accountId, email, accountPassword, accountCity, accountState]);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,6 @@
+let name = "pritam";
+let age = 21;
+let isloggedIn = false;
+let state = null;
+
+console.log();
